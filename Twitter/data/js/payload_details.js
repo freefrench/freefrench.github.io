7fb5e24f6084eb9e16fc80a26c4c93acf731acca
freefrench <34857852+freefrench@users.noreply.github.com>
2018-02-02 00:19:34 +0000

    Added 2 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 125360,
-	"created_at": "Thu Feb 01 00:19:34 +0000 2018",
+	"tweets": 125362,
+	"created_at": "Fri Feb 02 00:19:33 +0000 2018",
 	"lang": "en"
 }
---
+++ Twitter/data/js/tweets/2018_02.js
@@ -0,0 +1,484 @@
+Grailbird.data.tweets_2018_02 = 
+[
+	{
+		"created_at": "Thu Feb 01 21:53:37 +0000 2018",
+		"id": 959182976608276500,
+		"id_str": "959182976608276480",
+		"text": "A yoga class, earlier. https://t.co/jfRbUSmbCs https://t.co/rUp9eBfmYD",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jfRbUSmbCs",
+					"expanded_url": "https://twitter.com/MattWalshBlog/status/959079889302376449",
+					"display_url": "twitter.com/MattWalshBlog/…",
+					"indices": [
+						23,
+						46
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 959182969649991700,
+					"id_str": "959182969649991680",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/DU-z1DSXkAAPBxy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU-z1DSXkAAPBxy.jpg",
+					"url": "https://t.co/rUp9eBfmYD",
+					"display_url": "pic.twitter.com/rUp9eBfmYD",
+					"expanded_url": "https://twitter.com/rhodri/status/959182976608276480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 959182969649991700,
+					"id_str": "959182969649991680",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/DU-z1DSXkAAPBxy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU-z1DSXkAAPBxy.jpg",
+					"url": "https://t.co/rUp9eBfmYD",
+					"display_url": "pic.twitter.com/rUp9eBfmYD",
+					"expanded_url": "https://twitter.com/rhodri/status/959182976608276480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 345,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Done a Xmas book: https://t.co/m3ALXIOtvB.🎄Play in Scritti Politti, Dream Themes, Prescott. Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/m3ALXIOtvB",
+							"expanded_url": "https://buff.ly/2A1toAq",
+							"display_url": "buff.ly/2A1toAq",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 50598,
+			"friends_count": 5236,
+			"listed_count": 1068,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 5873,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 125301,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1505474382",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 959079889302376400,
+		"quoted_status_id_str": "959079889302376449",
+		"quoted_status": {
+			"created_at": "Thu Feb 01 15:03:59 +0000 2018",
+			"id": 959079889302376400,
+			"id_str": "959079889302376449",
+			"text": "The best comparison for Yoga would be the Ouija board. Yes you can play it \"just for fun\" without any ill intent, b… https://t.co/zsWcNODl7J",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/zsWcNODl7J",
+						"expanded_url": "https://twitter.com/i/web/status/959079889302376449",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 959070593961332700,
+			"in_reply_to_status_id_str": "959070593961332736",
+			"in_reply_to_user_id": 1500129642,
+			"in_reply_to_user_id_str": "1500129642",
+			"in_reply_to_screen_name": "MattWalshBlog",
+			"user": {
+				"id": 1500129642,
+				"id_str": "1500129642",
+				"name": "Matt Walsh",
+				"screen_name": "MattWalshBlog",
+				"location": "",
+				"description": "Writer, speaker, father of three, alpaca grooming expert, author of The Unholy Trinity. Contact@TheMattWalshBlog.com",
+				"url": "https://t.co/zfufKvWWIF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/zfufKvWWIF",
+								"expanded_url": "http://unholytrinitybook.com/books/the-unholy-trinity-hc",
+								"display_url": "unholytrinitybook.com/books/the-unho…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 131578,
+				"friends_count": 405,
+				"listed_count": 1700,
+				"created_at": "Tue Jun 11 03:17:16 +0000 2013",
+				"favourites_count": 4,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 23450,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/882606463562252289/98bgcZSD_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/882606463562252289/98bgcZSD_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1500129642/1491576836",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 179,
+			"favorite_count": 653,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Feb 01 18:15:28 +0000 2018",
+		"id": 959128081125519400,
+		"id_str": "959128081125519360",
+		"text": "@M_Z_Harrison  https://t.co/CcaYV7y614",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "M_Z_Harrison",
+					"name": "Melissa Harrison 🌾",
+					"id": 207032120,
+					"id_str": "207032120",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 959128075303882800,
+					"id_str": "959128075303882753",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/DU-B5x6XkAEobbg.png",
+					"media_url_https": "https://pbs.twimg.com/media/DU-B5x6XkAEobbg.png",
+					"url": "https://t.co/CcaYV7y614",
+					"display_url": "pic.twitter.com/CcaYV7y614",
+					"expanded_url": "https://twitter.com/rhodri/status/959128081125519360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 959128075303882800,
+					"id_str": "959128075303882753",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/DU-B5x6XkAEobbg.png",
+					"media_url_https": "https://pbs.twimg.com/media/DU-B5x6XkAEobbg.png",
+					"url": "https://t.co/CcaYV7y614",
+					"display_url": "pic.twitter.com/CcaYV7y614",
+					"expanded_url": "https://twitter.com/rhodri/status/959128081125519360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 398,
+							"h": 398,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 959127352025538600,
+		"in_reply_to_status_id_str": "959127352025538561",
+		"in_reply_to_user_id": 207032120,
+		"in_reply_to_user_id_str": "207032120",
+		"in_reply_to_screen_name": "M_Z_Harrison",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Done a Xmas book: https://t.co/m3ALXIOtvB.🎄Play in Scritti Politti, Dream Themes, Prescott. Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/m3ALXIOtvB",
+							"expanded_url": "https://buff.ly/2A1toAq",
+							"display_url": "buff.ly/2A1toAq",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 50598,
+			"friends_count": 5236,
+			"listed_count": 1068,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 5873,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 125301,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1505474382",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	}
+]
