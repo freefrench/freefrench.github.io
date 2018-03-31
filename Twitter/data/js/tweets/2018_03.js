0dbbf4c15293e904146dd4714a99d167a2f53272
freefrench <34857852+freefrench@users.noreply.github.com>
2018-03-31 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/tweets/2018_03.js
+++ Twitter/data/js/tweets/2018_03.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Fri Mar 30 19:39:14 +0000 2018",
+		"id": 979805270074363900,
+		"id_str": "979805270074363904",
+		"text": "I find it very soothing when these aquarium things pop up in my timeline.    I mean, no one could reply to THIS wit… https://t.co/PthTwM2DGR",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PthTwM2DGR",
+					"expanded_url": "https://twitter.com/i/web/status/979805270074363904",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write words, play tunes (Scritti Politti, Dream Themes, Prescott, Lost Crowns, Microdisney.) Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50211,
+			"friends_count": 5256,
+			"listed_count": 1071,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 6179,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 125435,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/173932663/twit-ground4.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 979803835576168400,
+		"quoted_status_id_str": "979803835576168448",
+		"quoted_status": {
+			"created_at": "Fri Mar 30 19:33:32 +0000 2018",
+			"id": 979803835576168400,
+			"id_str": "979803835576168448",
+			"text": "🐬　　　\n　　🐟　　　　　　　\n　　　　　　　 🐠　　\n 🐬　　　 🐟　　　 🐟　\n　　　　　　　　　　\n🌿　　　🌱🌱　🌿　🌱",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/EmojiAquarium\" rel=\"nofollow\">EmojiAquarium</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 829095470873997300,
+				"id_str": "829095470873997317",
+				"name": "Emoji Aquarium",
+				"screen_name": "EmojiAquarium",
+				"location": "Underwater",
+				"description": "I am a bot. Every few hours I show you a tiny aquarium full of interesting fishies. Please do not tap the glass. #botALLY. By @JoeSondow",
+				"url": "https://t.co/l8YjoK5AKV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/l8YjoK5AKV",
+								"expanded_url": "https://gitlab.com/JoeSondow/fishies/tree/master/src/main/java/sondow/fishies",
+								"display_url": "gitlab.com/JoeSondow/fish…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 12459,
+				"friends_count": 2,
+				"listed_count": 193,
+				"created_at": "Tue Feb 07 22:32:18 +0000 2017",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3270,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/832001745228148736/dlh__iqS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/832001745228148736/dlh__iqS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/829095470873997317/1487199485",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 29,
+			"favorite_count": 71,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "und"
+		},
+		"retweet_count": 11,
+		"favorite_count": 55,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 29 22:09:03 +0000 2018",
 		"id": 979480584887074800,
 		"id_str": "979480584887074816",
