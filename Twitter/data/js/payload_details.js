4b75448d31f6f6a2d05f6ae4dee1e751f522438f
freefrench <34857852+freefrench@users.noreply.github.com>
2018-07-30 01:19:35 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 126258,
-	"created_at": "Sun Jul 29 00:19:34 +0000 2018",
+	"tweets": 126259,
+	"created_at": "Mon Jul 30 00:19:34 +0000 2018",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2018_07.js
+++ Twitter/data/js/tweets/2018_07.js
@@ -1,6 +1,127 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Sun Jul 29 07:09:07 +0000 2018",
+		"id": 1023465425881968600,
+		"id_str": "1023465425881968641",
+		"text": "@nickinko @MarkW06 Raise The Girl / Can’t Be Beat / Adventure Of Dog / Summer In My Lunchtime / Jobless Youngsters… https://t.co/G4lnJ7Rnng",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nickinko",
+					"name": "nickinko",
+					"id": 21563623,
+					"id_str": "21563623",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "MarkW06",
+					"name": "Mark Williams",
+					"id": 3752244509,
+					"id_str": "3752244509",
+					"indices": [
+						10,
+						18
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/G4lnJ7Rnng",
+					"expanded_url": "https://twitter.com/i/web/status/1023465425881968641",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1023456480278798300,
+		"in_reply_to_status_id_str": "1023456480278798336",
+		"in_reply_to_user_id": 21563623,
+		"in_reply_to_user_id_str": "21563623",
+		"in_reply_to_screen_name": "nickinko",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writes words on demand, plays tunes (Scritti Politti, Dream Themes, Prescott, Lost Crowns.) Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 49221,
+			"friends_count": 5343,
+			"listed_count": 1057,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 7038,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 126048,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Jul 29 00:04:15 +0000 2018",
 		"id": 1023358506622234600,
 		"id_str": "1023358506622234624",
