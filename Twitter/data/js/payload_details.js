24787777be4b2cd52f63e6277b56c5faf7e7c52f
freefrench <34857852+freefrench@users.noreply.github.com>
2019-03-08 00:19:33 +0000

    Added 2 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 127499,
-	"created_at": "Thu Mar 07 00:19:33 +0000 2019",
+	"tweets": 127501,
+	"created_at": "Fri Mar 08 00:19:32 +0000 2019",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2019_03.js
+++ Twitter/data/js/tweets/2019_03.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 07 15:06:46 +0000 2019",
+		"id": 1103673349203509200,
+		"id_str": "1103673349203509248",
+		"full_text": "@iseetigers Nope, it's pretty much right: https://t.co/dcwdXrZqr9",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iseetigers",
+					"name": "Brian Gubb THE CHAIRMAN",
+					"id": 65692773,
+					"id_str": "65692773",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103673340378705900,
+					"id_str": "1103673340378705922",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/D1EJDjlXcAIc6Fl.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1EJDjlXcAIc6Fl.png",
+					"url": "https://t.co/dcwdXrZqr9",
+					"display_url": "pic.twitter.com/dcwdXrZqr9",
+					"expanded_url": "https://twitter.com/rhodri/status/1103673349203509248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103673340378705900,
+					"id_str": "1103673340378705922",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/D1EJDjlXcAIc6Fl.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1EJDjlXcAIc6Fl.png",
+					"url": "https://t.co/dcwdXrZqr9",
+					"display_url": "pic.twitter.com/dcwdXrZqr9",
+					"expanded_url": "https://twitter.com/rhodri/status/1103673349203509248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 469,
+							"h": 210,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1103667951180357600,
+		"in_reply_to_status_id_str": "1103667951180357632",
+		"in_reply_to_user_id": 65692773,
+		"in_reply_to_user_id_str": "65692773",
+		"in_reply_to_screen_name": "iseetigers",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writes words, emits tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52441,
+			"friends_count": 5599,
+			"listed_count": 1042,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 8452,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 127114,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@iseetigers Nope, it's pretty much right: https://t.co/dcwdXrZqr9"
+	},
+	{
+		"created_at": "Thu Mar 07 14:42:13 +0000 2019",
+		"id": 1103667173174714400,
+		"id_str": "1103667173174714368",
+		"full_text": "@twelsh10 Trevor!!! Some Mothers Do Ave 'Em! Come on! :)",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "twelsh10",
+					"name": "Trevor Welsh",
+					"id": 22053473,
+					"id_str": "22053473",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1103666268077473800,
+		"in_reply_to_status_id_str": "1103666268077473795",
+		"in_reply_to_user_id": 22053473,
+		"in_reply_to_user_id_str": "22053473",
+		"in_reply_to_screen_name": "twelsh10",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writes words, emits tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Not the unworst person you've never not met.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52441,
+			"friends_count": 5599,
+			"listed_count": 1042,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 8452,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 127114,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "9B0B00",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@twelsh10 Trevor!!! Some Mothers Do Ave 'Em! Come on! :)"
+	},
+	{
 		"created_at": "Wed Mar 06 11:02:38 +0000 2019",
 		"id": 1103249522304708600,
 		"id_str": "1103249522304708608",
