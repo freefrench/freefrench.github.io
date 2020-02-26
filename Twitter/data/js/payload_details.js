615fc5a5d62c25a5d2eb247bd672b6327688e0a3
freefrench <34857852+freefrench@users.noreply.github.com>
2020-02-26 00:19:38 +0000

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 130601,
-	"created_at": "Tue Feb 25 00:19:35 +0000 2020",
+	"tweets": 130602,
+	"created_at": "Wed Feb 26 00:19:36 +0000 2020",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2020_02.js
+++ Twitter/data/js/tweets/2020_02.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 25 10:05:55 +0000 2020",
+		"id": 1232245329665364000,
+		"id_str": "1232245329665363968",
+		"full_text": "@PGPowell @MerylORourke I do wonder whether that site is useful or not, tho. I wrote a thing about petitions a few years ago, and I remember interviewing a campaigner who thought it was bullshit. A desperate attempt by government to synthesise a trust in politics.",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PGPowell",
+					"name": "Paul Powell",
+					"id": 21749953,
+					"id_str": "21749953",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "MerylORourke",
+					"name": "Meryl O'Rourke",
+					"id": 20373981,
+					"id_str": "20373981",
+					"indices": [
+						10,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1232243256953593900,
+		"in_reply_to_status_id_str": "1232243256953593858",
+		"in_reply_to_user_id": 21749953,
+		"in_reply_to_user_id_str": "21749953",
+		"in_reply_to_screen_name": "PGPowell",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Did a Brexit disco album.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63693,
+			"friends_count": 6451,
+			"listed_count": 1036,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 14017,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 130369,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@PGPowell @MerylORourke I do wonder whether that site is useful or not, tho. I wrote a thing about petitions a few years ago, and I remember interviewing a campaigner who thought it was bullshit. A desperate attempt by government to synthesise a trust in politics."
+	},
+	{
 		"created_at": "Mon Feb 24 20:21:57 +0000 2020",
 		"id": 1231974515950997500,
 		"id_str": "1232037971215421441",
