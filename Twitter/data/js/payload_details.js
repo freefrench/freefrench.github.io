add99a500a1840bb55f3ac15dd59a27eb976e23b
freefrench <34857852+freefrench@users.noreply.github.com>
2020-05-14 01:19:35 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 131587,
-	"created_at": "Wed May 13 00:19:34 +0000 2020",
+	"tweets": 131588,
+	"created_at": "Thu May 14 00:19:34 +0000 2020",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2020_05.js
+++ Twitter/data/js/tweets/2020_05.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 13 07:21:11 +0000 2020",
+		"id": 1260470126266245000,
+		"id_str": "1260470126266245124",
+		"full_text": "@CraddocksFanny Yeah, the gate is my favourite bit! And you can hear it!",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CraddocksFanny",
+					"name": "Boing",
+					"id": 2763759514,
+					"id_str": "2763759514",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1260467224600547300,
+		"in_reply_to_status_id_str": "1260467224600547329",
+		"in_reply_to_user_id": 2763759514,
+		"in_reply_to_user_id_str": "2763759514",
+		"in_reply_to_screen_name": "CraddocksFanny",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63838,
+			"friends_count": 6340,
+			"listed_count": 949,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 15861,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 131399,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@CraddocksFanny Yeah, the gate is my favourite bit! And you can hear it!"
+	},
+	{
 		"created_at": "Tue May 12 21:04:57 +0000 2020",
 		"id": 1260315048334983200,
 		"id_str": "1260315048334983170",
