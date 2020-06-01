cdf39806fa33d7710e98fa12358a89b4c4c5281a
freefrench <34857852+freefrench@users.noreply.github.com>
2020-06-01 01:19:36 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 131776,
-	"created_at": "Sun May 31 00:19:36 +0000 2020",
+	"tweets": 131777,
+	"created_at": "Mon Jun 01 00:19:35 +0000 2020",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2020_05.js
+++ Twitter/data/js/tweets/2020_05.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 21:51:31 +0000 2020",
+		"id": 1267212135114956800,
+		"id_str": "1267212135114956803",
+		"full_text": "@SquashGrill I did a talk on it at @boringconferenc and then played it throughout the lunch hour while people mooched off to buy sandwiches.",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SquashGrill",
+					"name": "Josh Perl",
+					"id": 822074125225226200,
+					"id_str": "822074125225226240",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "boringconferenc",
+					"name": "Boring Conference",
+					"id": 199605416,
+					"id_str": "199605416",
+					"indices": [
+						35,
+						51
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1267165510388584400,
+		"in_reply_to_status_id_str": "1267165510388584452",
+		"in_reply_to_user_id": 822074125225226200,
+		"in_reply_to_user_id_str": "822074125225226240",
+		"in_reply_to_screen_name": "SquashGrill",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63795,
+			"friends_count": 6362,
+			"listed_count": 947,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 16329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 131598,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SquashGrill I did a talk on it at @boringconferenc and then played it throughout the lunch hour while people mooched off to buy sandwiches."
+	},
+	{
 		"created_at": "Sat May 30 21:15:56 +0000 2020",
 		"id": 1266840792653586400,
 		"id_str": "1266840792653586432",
