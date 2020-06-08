b60ea43933abd6fc39c3deaeeed612243cba3be3
freefrench <34857852+freefrench@users.noreply.github.com>
2020-06-08 01:19:35 +0100

    Added 3 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 131836,
-	"created_at": "Sun Jun 07 00:19:34 +0000 2020",
+	"tweets": 131839,
+	"created_at": "Mon Jun 08 00:19:34 +0000 2020",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2020_06.js
+++ Twitter/data/js/tweets/2020_06.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 07 19:34:21 +0000 2020",
+		"id": 1269714129746456600,
+		"id_str": "1269714330230153216",
+		"full_text": "🐡　　　　　　　\n　　　　　　　 🐡　　\n　　　　　　　　　　\n　 🐟　 🐟　　🐡　　 🐡\n　　🐟　　　　　　　\n　　　　🌿🌱　🌱　🌾",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/EmojiAquarium\" rel=\"nofollow\">EmojiAquarium</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 829095470873997300,
+			"id_str": "829095470873997317",
+			"name": "Emoji Aquarium",
+			"screen_name": "EmojiAquarium",
+			"location": "Underwater",
+			"description": "I am a bot created by @JoeSondow. Every few hours I show you a tiny aquarium full of interesting fishies. Please do not tap the glass.",
+			"url": "https://t.co/l8YjoK5AKV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/l8YjoK5AKV",
+							"expanded_url": "https://gitlab.com/JoeSondow/fishies/tree/master/src/main/java/sondow/fishies",
+							"display_url": "gitlab.com/JoeSondow/fish…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 25012,
+			"friends_count": 4,
+			"listed_count": 364,
+			"created_at": "Tue Feb 07 22:32:18 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9710,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/832001745228148736/dlh__iqS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/832001745228148736/dlh__iqS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/829095470873997317/1487199485",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 92,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "und",
+		"text": "🐡　　　　　　　\n　　　　　　　 🐡　　\n　　　　　　　　　　\n　 🐟　 🐟　　🐡　　 🐡\n　　🐟　　　　　　　\n　　　　🌿🌱　🌱　🌾"
+	},
+	{
+		"created_at": "Sun Jun 07 18:45:46 +0000 2020",
+		"id": 1269702103800430600,
+		"id_str": "1269702103800430592",
+		"full_text": "@kalenski They’ll be back! We had nothing for weeks and now they’re DESCENDING",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kalenski",
+					"name": "(((kalen)))",
+					"id": 24538779,
+					"id_str": "24538779",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1269701527544844300,
+		"in_reply_to_status_id_str": "1269701527544844288",
+		"in_reply_to_user_id": 24538779,
+		"in_reply_to_user_id_str": "24538779",
+		"in_reply_to_screen_name": "kalenski",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63742,
+			"friends_count": 6363,
+			"listed_count": 946,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 16460,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 131659,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@kalenski They’ll be back! We had nothing for weeks and now they’re DESCENDING"
+	},
+	{
+		"created_at": "Sun Jun 07 18:41:43 +0000 2020",
+		"id": 1269701085083680800,
+		"id_str": "1269701085083680778",
+		"full_text": "GOLDFINCH ON BIRD FEEDER ALERT https://t.co/vldPNkhHvY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			54
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vldPNkhHvY",
+					"expanded_url": "https://www.instagram.com/p/CBJPBfKnQs4/?igshid=wayg9ilm4njh",
+					"display_url": "instagram.com/p/CBJPBfKnQs4/…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63742,
+			"friends_count": 6363,
+			"listed_count": 946,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 16460,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 131659,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 80,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "GOLDFINCH ON BIRD FEEDER ALERT https://t.co/vldPNkhHvY"
+	},
+	{
 		"created_at": "Sat Jun 06 19:18:21 +0000 2020",
 		"id": 1269260314547273700,
 		"id_str": "1269347916080123906",
