9f386dabfafb683e73b7ae1888516ebeff17683c
freefrench <34857852+freefrench@users.noreply.github.com>
2021-04-18 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 134693,
-	"created_at": "Fri Apr 16 00:19:33 +0000 2021",
+	"tweets": 134694,
+	"created_at": "Sun Apr 18 00:19:33 +0000 2021",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2021_04.js
+++ Twitter/data/js/tweets/2021_04.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Fri Apr 16 17:32:11 +0000 2021",
+		"id": 1383110987918758000,
+		"id_str": "1383110987918757888",
+		"full_text": "@SwimmersJackson @realcathalc @LukeHaines_News Enjoyed this!",
+		"truncated": false,
+		"display_text_range": [
+			47,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SwimmersJackson",
+					"name": "Swimmers Jackson",
+					"id": 198119119,
+					"id_str": "198119119",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "realcathalc",
+					"name": "Cathal Coughlan/Co-Aklan",
+					"id": 15916207,
+					"id_str": "15916207",
+					"indices": [
+						17,
+						29
+					]
+				},
+				{
+					"screen_name": "LukeHaines_News",
+					"name": "Luke Haines Rock N Roll",
+					"id": 221762327,
+					"id_str": "221762327",
+					"indices": [
+						30,
+						46
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1382959695950938000,
+		"in_reply_to_status_id_str": "1382959695950938112",
+		"in_reply_to_user_id": 198119119,
+		"in_reply_to_user_id_str": "198119119",
+		"in_reply_to_screen_name": "SwimmersJackson",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 66498,
+			"friends_count": 6689,
+			"listed_count": 935,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 25244,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 134647,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SwimmersJackson @realcathalc @LukeHaines_News Enjoyed this!"
+	},
+	{
 		"created_at": "Thu Apr 15 21:47:23 +0000 2021",
 		"id": 1382812826083991600,
 		"id_str": "1382812826083991552",
