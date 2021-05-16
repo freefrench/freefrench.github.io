dfc18aa3a27acd076458cee09243fd47be3cea7b
freefrench <34857852+freefrench@users.noreply.github.com>
2021-05-16 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 134950,
-	"created_at": "Sat May 15 00:19:33 +0000 2021",
+	"tweets": 134951,
+	"created_at": "Sun May 16 00:19:33 +0000 2021",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2021_05.js
+++ Twitter/data/js/tweets/2021_05.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 15 12:23:38 +0000 2021",
+		"id": 1393542589492523000,
+		"id_str": "1393542589492523017",
+		"full_text": "@lisamarkwell Chesham Arms on Mehetabel Rd, E9. (Already loved that pub in any case.)",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lisamarkwell",
+					"name": "Lisa Markwell",
+					"id": 135195122,
+					"id_str": "135195122",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1393537553312190500,
+		"in_reply_to_status_id_str": "1393537553312190468",
+		"in_reply_to_user_id": 135195122,
+		"in_reply_to_user_id_str": "135195122",
+		"in_reply_to_screen_name": "lisamarkwell",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 66777,
+			"friends_count": 6717,
+			"listed_count": 931,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 26190,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 134923,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@lisamarkwell Chesham Arms on Mehetabel Rd, E9. (Already loved that pub in any case.)"
+	},
+	{
 		"created_at": "Fri May 14 19:08:30 +0000 2021",
 		"id": 1393282089571717000,
 		"id_str": "1393282089571717121",
