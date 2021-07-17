53c7edcb0154349f4bd231fc0fae0a408ee36c4a
freefrench <34857852+freefrench@users.noreply.github.com>
2021-07-17 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/tweets/2021_07.js
+++ Twitter/data/js/tweets/2021_07.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 16 21:20:18 +0000 2021",
+		"id": 1416145692016488400,
+		"id_str": "1416145692016488448",
+		"full_text": "@JudithFlanders @tim_brannigan I’ve been watching Shtisel on Netflix and now I’m really getting the hang of mazel tov.",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			118
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JudithFlanders",
+					"name": "Judith Flanders",
+					"id": 256422545,
+					"id_str": "256422545",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "tim_brannigan",
+					"name": "Tim Brannigan",
+					"id": 256180339,
+					"id_str": "256180339",
+					"indices": [
+						16,
+						30
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1416145188259602400,
+		"in_reply_to_status_id_str": "1416145188259602433",
+		"in_reply_to_user_id": 256422545,
+		"in_reply_to_user_id_str": "256422545",
+		"in_reply_to_screen_name": "JudithFlanders",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 68085,
+			"friends_count": 6796,
+			"listed_count": 938,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 27474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 135374,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@JudithFlanders @tim_brannigan I’ve been watching Shtisel on Netflix and now I’m really getting the hang of mazel tov."
+	},
+	{
 		"created_at": "Thu Jul 15 20:38:14 +0000 2021",
 		"id": 1415772716746133500,
 		"id_str": "1415772716746133507",
