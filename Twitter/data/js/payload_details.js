c0df32926c36d3707f2b0d17480c7df71ec9ed63
freefrench <34857852+freefrench@users.noreply.github.com>
2021-09-20 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 135869,
-	"created_at": "Sun Sep 19 00:19:33 +0000 2021",
+	"tweets": 135870,
+	"created_at": "Mon Sep 20 00:19:33 +0000 2021",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2021_09.js
+++ Twitter/data/js/tweets/2021_09.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 19 21:13:22 +0000 2021",
+		"id": 1439699160178040800,
+		"id_str": "1439699160178040834",
+		"full_text": "@rsmck @msrachelramsay The best and most comprehensive explanation of why I think Ross is right: https://t.co/prumNjZYlW",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rsmck",
+					"name": "Ross McKillop ",
+					"id": 14089880,
+					"id_str": "14089880",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "msrachelramsay",
+					"name": "Rachel Ramsay",
+					"id": 19246645,
+					"id_str": "19246645",
+					"indices": [
+						7,
+						22
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/prumNjZYlW",
+					"expanded_url": "https://www.wired.com/story/facebooks-listening-smartphone-microphone/",
+					"display_url": "wired.com/story/facebook…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1439697365015933000,
+		"in_reply_to_status_id_str": "1439697365015932932",
+		"in_reply_to_user_id": 14089880,
+		"in_reply_to_user_id_str": "14089880",
+		"in_reply_to_screen_name": "rsmck",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit tunes (Scritti Politti, Dream Themes, Lost Crowns, Kenny Process Team). Capable, reliable, pliable.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 68824,
+			"friends_count": 6826,
+			"listed_count": 934,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 28936,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 135833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@rsmck @msrachelramsay The best and most comprehensive explanation of why I think Ross is right: https://t.co/prumNjZYlW"
+	},
+	{
 		"created_at": "Sat Sep 18 19:00:26 +0000 2021",
 		"id": 1439303319437119500,
 		"id_str": "1439303319437119490",
