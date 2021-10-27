8677cfe033dae386867dd3b896e256db232733d3
freefrench <34857852+freefrench@users.noreply.github.com>
2021-10-27 01:19:36 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 136137,
-	"created_at": "Tue Oct 26 00:19:34 +0000 2021",
+	"tweets": 136138,
+	"created_at": "Wed Oct 27 00:19:35 +0000 2021",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2021_10.js
+++ Twitter/data/js/tweets/2021_10.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Tue Oct 26 22:04:15 +0000 2021",
+		"id": 1453120314997624800,
+		"id_str": "1453120314997624838",
+		"full_text": "@dickon_edwards Isn’t it gorgeous? Grab the album, it’s a total delight ❤️",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dickon_edwards",
+					"name": "Dickon Edwards",
+					"id": 7774522,
+					"id_str": "7774522",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1453112395581243400,
+		"in_reply_to_status_id_str": "1453112395581243399",
+		"in_reply_to_user_id": 7774522,
+		"in_reply_to_user_id_str": "7774522",
+		"in_reply_to_screen_name": "dickon_edwards",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit sound (Scritti Politti, Kenny Process Team, Lost Crowns, Dream Themes.) Capable, reliable, pliable, available.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69107,
+			"friends_count": 6851,
+			"listed_count": 930,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 29950,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 136086,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@dickon_edwards Isn’t it gorgeous? Grab the album, it’s a total delight ❤️"
+	},
+	{
 		"created_at": "Mon Oct 25 22:38:53 +0000 2021",
 		"id": 1452766642597998600,
 		"id_str": "1452766642597998597",
@@ -212,7 +319,7 @@ Grailbird.data.tweets_2021_10 =
 			"user_mentions": [
 				{
 					"screen_name": "claz0r",
-					"name": "S-Claire-y Screen Name 🏳️‍🌈💖💜💙🤖",
+					"name": "S-Claire-y Screen Name 🏳️🌈💖💜💙🤖",
 					"id": 72831028,
 					"id_str": "72831028",
 					"indices": [
