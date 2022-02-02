fa7774ad75d8a7bcef2af27f476b846cefba8671
freefrench <34857852+freefrench@users.noreply.github.com>
2022-02-02 00:19:35 +0000

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 137187,
-	"created_at": "Tue Feb 01 00:19:33 +0000 2022",
+	"tweets": 137188,
+	"created_at": "Wed Feb 02 00:19:34 +0000 2022",
 	"lang": "en"
 }
---
+++ Twitter/data/js/tweets/2022_02.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 01 13:13:25 +0000 2022",
+		"id": 1488500736778457000,
+		"id_str": "1488500736778457088",
+		"full_text": "@_Poots_ @silverpebble Both Susannah and I have turned to mush. S has a PhD viva in a fortnight. LOLOLOL",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_Poots_",
+					"name": "Olivia Potts",
+					"id": 14441480,
+					"id_str": "14441480",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "silverpebble",
+					"name": "Emma Mitchell 💙",
+					"id": 245402039,
+					"id_str": "245402039",
+					"indices": [
+						9,
+						22
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1488499589598568400,
+		"in_reply_to_status_id_str": "1488499589598568452",
+		"in_reply_to_user_id": 14441480,
+		"in_reply_to_user_id_str": "14441480",
+		"in_reply_to_screen_name": "_Poots_",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit sound (Scritti Politti, Kenny Process Team, Lost Crowns, Dream Themes.) Capable, reliable, pliable, available.",
+			"url": "https://t.co/aoc7DmVmLD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aoc7DmVmLD",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71357,
+			"friends_count": 7005,
+			"listed_count": 934,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 33048,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 137189,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@_Poots_ @silverpebble Both Susannah and I have turned to mush. S has a PhD viva in a fortnight. LOLOLOL"
+	}
+]
