22968ddfb53a0637c3ab65fb1e160dce4a4ccb66
freefrench <34857852+freefrench@users.noreply.github.com>
2022-05-25 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 137931,
-	"created_at": "Tue May 24 00:19:33 +0000 2022",
+	"tweets": 137932,
+	"created_at": "Wed May 25 00:19:33 +0000 2022",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2022_05.js
+++ Twitter/data/js/tweets/2022_05.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 24 05:46:01 +0000 2022",
+		"id": 1528975585212604400,
+		"id_str": "1528975585212604416",
+		"full_text": "When you hear of a friend’s death, and while processing this you go to your phone to revisit messages you exchanged, and at the top it says their name, “last seen at”, and a date and time, that creates a poignancy which was certainly not intended by the creators of WhatsApp.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit sound: Scritti Politti, Kenny Process Team, Lost Crowns, Dream Themes. Valuable, reliable, affable, available.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71706,
+			"friends_count": 7076,
+			"listed_count": 926,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 35034,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 137966,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 644,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "When you hear of a friend’s death, and while processing this you go to your phone to revisit messages you exchanged, and at the top it says their name, “last seen at”, and a date and time, that creates a poignancy which was certainly not intended by the creators of WhatsApp."
+	},
+	{
 		"created_at": "Mon May 23 17:58:35 +0000 2022",
 		"id": 1528797556817395700,
 		"id_str": "1528797556817395714",
