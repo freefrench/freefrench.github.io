cfa028d1077eab520ea6643767a2d7ff107def6f
freefrench <34857852+freefrench@users.noreply.github.com>
2022-06-11 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 137999,
-	"created_at": "Fri Jun 10 00:19:33 +0000 2022",
+	"tweets": 138000,
+	"created_at": "Sat Jun 11 00:19:33 +0000 2022",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2022_06.js
+++ Twitter/data/js/tweets/2022_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 10 07:34:11 +0000 2022",
+		"id": 1535163402611564500,
+		"id_str": "1535163402611564544",
+		"full_text": "@NorthernWrites @great_electric Can I’m v familiar with but for some reason Neu passed me by",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NorthernWrites",
+					"name": "Chris King",
+					"id": 19541062,
+					"id_str": "19541062",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "great_electric",
+					"name": "The Great Electric",
+					"id": 1442198084,
+					"id_str": "1442198084",
+					"indices": [
+						16,
+						31
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1535162427637219300,
+		"in_reply_to_status_id_str": "1535162427637219335",
+		"in_reply_to_user_id": 19541062,
+		"in_reply_to_user_id_str": "19541062",
+		"in_reply_to_screen_name": "NorthernWrites",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit sound: Scritti Politti, Kenny Process Team, Lost Crowns, Dream Themes. Valuable, reliable, affable, available.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71791,
+			"friends_count": 7079,
+			"listed_count": 926,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 35259,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 138042,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@NorthernWrites @great_electric Can I’m v familiar with but for some reason Neu passed me by"
+	},
+	{
 		"created_at": "Thu Jun 09 17:49:03 +0000 2022",
 		"id": 1534955751185207300,
 		"id_str": "1534955751185207305",
