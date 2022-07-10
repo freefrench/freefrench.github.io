5c1cd5b6501e48c482240e33e7e2be22632afd61
freefrench <34857852+freefrench@users.noreply.github.com>
2022-07-10 01:19:33 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 138090,
-	"created_at": "Sat Jul 09 00:19:32 +0000 2022",
+	"tweets": 138091,
+	"created_at": "Sun Jul 10 00:19:32 +0000 2022",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2022_07.js
+++ Twitter/data/js/tweets/2022_07.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sat Jul 09 07:28:35 +0000 2022",
+		"id": 1545671239431344000,
+		"id_str": "1545671239431344129",
+		"full_text": "@PatrickEFriel @lamarr_mark Love playing saw. I didn’t practice for all that time for nothing!",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PatrickEFriel",
+					"name": "Patrick E Friel",
+					"id": 332300333,
+					"id_str": "332300333",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "lamarr_mark",
+					"name": "Mark Lamarr",
+					"id": 1011619611698257900,
+					"id_str": "1011619611698257920",
+					"indices": [
+						15,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1545668367482232800,
+		"in_reply_to_status_id_str": "1545668367482232832",
+		"in_reply_to_user_id": 332300333,
+		"in_reply_to_user_id_str": "332300333",
+		"in_reply_to_screen_name": "PatrickEFriel",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Write features & books. Emit sound: Scritti Politti, Kenny Process Team, Lost Crowns, Dream Themes. Valuable, reliable, affable, available.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71977,
+			"friends_count": 7108,
+			"listed_count": 926,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 35595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 138139,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@PatrickEFriel @lamarr_mark Love playing saw. I didn’t practice for all that time for nothing!"
+	},
+	{
 		"created_at": "Fri Jul 08 19:36:59 +0000 2022",
 		"id": 1545492159826206700,
 		"id_str": "1545492159826206720",
@@ -219,7 +336,7 @@ Grailbird.data.tweets_2022_07 =
 			"user_mentions": [
 				{
 					"screen_name": "CatieCraig",
-					"name": "Catie 🏳️‍⚧️🏳️‍🌈🇪🇺❤️",
+					"name": "Catie 🏳️⚧️🏳️🌈🇪🇺❤️",
 					"id": 20226700,
 					"id_str": "20226700",
 					"indices": [
