71d3cad721209c0045b9c6079964bc8dadd344eb
freefrench <34857852+freefrench@users.noreply.github.com>
2023-02-09 00:19:34 +0000

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 139256,
-	"created_at": "Wed Feb 08 00:19:33 +0000 2023",
+	"tweets": 139257,
+	"created_at": "Thu Feb 09 00:19:33 +0000 2023",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2023_02.js
+++ Twitter/data/js/tweets/2023_02.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Wed Feb 08 16:43:25 +0000 2023",
+		"id": 1623361870978551800,
+		"id_str": "1623361870978551808",
+		"full_text": "@andrewfarquhar @NickFaber Boom! The answer!!",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "andrewfarquhar",
+					"name": "Andrew Lee Farquhar",
+					"id": 3319512507,
+					"id_str": "3319512507",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "NickFaber",
+					"name": "Nick Faber",
+					"id": 19648718,
+					"id_str": "19648718",
+					"indices": [
+						16,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1623351397138813000,
+		"in_reply_to_status_id_str": "1623351397138812929",
+		"in_reply_to_user_id": 3319512507,
+		"in_reply_to_user_id_str": "3319512507",
+		"in_reply_to_screen_name": "andrewfarquhar",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist @HTSI. Play with Scritti Politti, Kenny Process Team, Lost Crowns. Mastodon: @rhodri@vivaldi.net",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72291,
+			"friends_count": 7125,
+			"listed_count": 913,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 38781,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 139385,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@andrewfarquhar @NickFaber Boom! The answer!!"
+	},
+	{
 		"created_at": "Tue Feb 07 23:42:33 +0000 2023",
 		"id": 1623104963118235600,
 		"id_str": "1623104963118235648",
