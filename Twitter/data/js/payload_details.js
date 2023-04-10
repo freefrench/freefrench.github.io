354fd775d42cd01b47a9bdd5242448e4093d6005
freefrench <34857852+freefrench@users.noreply.github.com>
2023-04-10 01:19:34 +0100

    Added 2 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 139549,
-	"created_at": "Sat Apr 08 00:19:33 +0000 2023",
+	"tweets": 139551,
+	"created_at": "Mon Apr 10 00:19:33 +0000 2023",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2023_04.js
+++ Twitter/data/js/tweets/2023_04.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 09 18:35:22 +0000 2023",
+		"id": 1645133318029975600,
+		"id_str": "1645133318029975557",
+		"full_text": "@TobyonTV Nope! I’ve not been in there yet, having lived here, um, two years",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TobyonTV",
+					"name": "Toby Earle 🇺🇦",
+					"id": 188495946,
+					"id_str": "188495946",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1645133052111093800,
+		"in_reply_to_status_id_str": "1645133052111093762",
+		"in_reply_to_user_id": 188495946,
+		"in_reply_to_user_id_str": "188495946",
+		"in_reply_to_screen_name": "TobyonTV",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist @HTSI. Rapidly ageing musician. Play with Scritti Politti, Kenny Process Team, Lost Crowns, Sweet Billy Pilgrim.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72275,
+			"friends_count": 7127,
+			"listed_count": 924,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 39419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 139717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@TobyonTV Nope! I’ve not been in there yet, having lived here, um, two years"
+	},
+	{
+		"created_at": "Sun Apr 09 18:31:39 +0000 2023",
+		"id": 1645132379965595600,
+		"id_str": "1645132379965595649",
+		"full_text": "Although, having said that, it’s a bit like doing a tongue twister to impress your mates. I might go over and say “hello, my sixth sheik’s sheep’s sick” and see if they like it.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1645131917279240200,
+		"in_reply_to_status_id_str": "1645131917279240193",
+		"in_reply_to_user_id": 1524841,
+		"in_reply_to_user_id_str": "1524841",
+		"in_reply_to_screen_name": "rhodri",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist @HTSI. Rapidly ageing musician. Play with Scritti Politti, Kenny Process Team, Lost Crowns, Sweet Billy Pilgrim.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72275,
+			"friends_count": 7127,
+			"listed_count": 924,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 39419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 139717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Although, having said that, it’s a bit like doing a tongue twister to impress your mates. I might go over and say “hello, my sixth sheik’s sheep’s sick” and see if they like it."
+	},
+	{
 		"created_at": "Fri Apr 07 20:10:53 +0000 2023",
 		"id": 1644432580408668200,
 		"id_str": "1644432580408668168",
@@ -1664,7 +1867,7 @@ Grailbird.data.tweets_2023_04 =
 		"created_at": "Fri Apr 07 09:16:15 +0000 2023",
 		"id": 1644267834183229400,
 		"id_str": "1644267834183229440",
-		"full_text": "I’d reply this tweet but I can’t because Twitter won’t let me because it has a substack link in it. 🤷🏻‍♂️ https://t.co/W9vjbjS39B",
+		"full_text": "I’d reply this tweet but I can’t because Twitter won’t let me because it has a substack link in it. 🤷🏻♂️ https://t.co/W9vjbjS39B",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -1894,7 +2097,7 @@ Grailbird.data.tweets_2023_04 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "I’d reply this tweet but I can’t because Twitter won’t let me because it has a substack link in it. 🤷🏻‍♂️ https://t.co/W9vjbjS39B"
+		"text": "I’d reply this tweet but I can’t because Twitter won’t let me because it has a substack link in it. 🤷🏻♂️ https://t.co/W9vjbjS39B"
 	},
 	{
 		"created_at": "Fri Apr 07 09:12:49 +0000 2023",
@@ -2343,7 +2546,7 @@ Grailbird.data.tweets_2023_04 =
 		"created_at": "Fri Apr 07 08:45:11 +0000 2023",
 		"id": 1644260014784143400,
 		"id_str": "1644260014784143360",
-		"full_text": "@cox_tom And, actually, I can RT other substack-linking tweets of yours. Just not that one. 🤷🏻‍♂️",
+		"full_text": "@cox_tom And, actually, I can RT other substack-linking tweets of yours. Just not that one. 🤷🏻♂️",
 		"truncated": false,
 		"display_text_range": [
 			9,
@@ -2444,7 +2647,7 @@ Grailbird.data.tweets_2023_04 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@cox_tom And, actually, I can RT other substack-linking tweets of yours. Just not that one. 🤷🏻‍♂️"
+		"text": "@cox_tom And, actually, I can RT other substack-linking tweets of yours. Just not that one. 🤷🏻♂️"
 	},
 	{
 		"created_at": "Thu Apr 06 20:20:34 +0000 2023",
