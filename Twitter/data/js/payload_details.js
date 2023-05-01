b249b4386a0a206617bf5ffc576193d301d33143
freefrench <34857852+freefrench@users.noreply.github.com>
2023-05-01 01:19:35 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 139691,
-	"created_at": "Sun Apr 30 00:19:34 +0000 2023",
+	"tweets": 139692,
+	"created_at": "Mon May 01 00:19:34 +0000 2023",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2023_04.js
+++ Twitter/data/js/tweets/2023_04.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 30 20:54:42 +0000 2023",
+		"id": 1652778527350030300,
+		"id_str": "1652778527350030336",
+		"full_text": "Braintree is really getting into the swing of the whole coronation thing. Special shout out to Barclays Bank for some exceptional Charles III colouring in. https://t.co/d4iVGZpnWc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1652778516981637000,
+					"id_str": "1652778516981637120",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/Fu_ZTe0WYAAtIN3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fu_ZTe0WYAAtIN3.jpg",
+					"url": "https://t.co/d4iVGZpnWc",
+					"display_url": "pic.twitter.com/d4iVGZpnWc",
+					"expanded_url": "https://twitter.com/rhodri/status/1652778527350030336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1652778516981637000,
+					"id_str": "1652778516981637120",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/Fu_ZTe0WYAAtIN3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fu_ZTe0WYAAtIN3.jpg",
+					"url": "https://t.co/d4iVGZpnWc",
+					"display_url": "pic.twitter.com/d4iVGZpnWc",
+					"expanded_url": "https://twitter.com/rhodri/status/1652778527350030336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1652778516981612500,
+					"id_str": "1652778516981612546",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/Fu_ZTe0WAAILMW8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fu_ZTe0WAAILMW8.jpg",
+					"url": "https://t.co/d4iVGZpnWc",
+					"display_url": "pic.twitter.com/d4iVGZpnWc",
+					"expanded_url": "https://twitter.com/rhodri/status/1652778527350030336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1652778516977533000,
+					"id_str": "1652778516977532929",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/Fu_ZTezXwAEyCn9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fu_ZTezXwAEyCn9.jpg",
+					"url": "https://t.co/d4iVGZpnWc",
+					"display_url": "pic.twitter.com/d4iVGZpnWc",
+					"expanded_url": "https://twitter.com/rhodri/status/1652778527350030336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1652778516973334500,
+					"id_str": "1652778516973334529",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/Fu_ZTeyXsAE63rx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fu_ZTeyXsAE63rx.jpg",
+					"url": "https://t.co/d4iVGZpnWc",
+					"display_url": "pic.twitter.com/d4iVGZpnWc",
+					"expanded_url": "https://twitter.com/rhodri/status/1652778527350030336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden ⏏️",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist @HTSI. Rapidly ageing musician. Play with Scritti Politti, Kenny Process Team, Lost Crowns, Sweet Billy Pilgrim.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72214,
+			"friends_count": 7118,
+			"listed_count": 918,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 39670,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 139872,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 16,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Braintree is really getting into the swing of the whole coronation thing. Special shout out to Barclays Bank for some exceptional Charles III colouring in. https://t.co/d4iVGZpnWc"
+	},
+	{
 		"created_at": "Sat Apr 29 19:16:28 +0000 2023",
 		"id": 1652391416906018800,
 		"id_str": "1652391416906018822",
