610b72e4cf1e54eeb4c4bdaa7e9dade2e2198020
freefrench <34857852+freefrench@users.noreply.github.com>
2023-05-13 01:19:34 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 139761,
-	"created_at": "Fri May 12 00:19:38 +0000 2023",
+	"tweets": 139762,
+	"created_at": "Sat May 13 00:19:33 +0000 2023",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2023_05.js
+++ Twitter/data/js/tweets/2023_05.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Fri May 12 08:47:19 +0000 2023",
+		"id": 1656944130595987500,
+		"id_str": "1656944130595987457",
+		"full_text": "I would urge lovers of wonderfully unusual films made on shoestring budgets to pop along to see Keith's film next month. A glorious achievement! ♥️ https://t.co/z3HUxqEoYw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/z3HUxqEoYw",
+					"expanded_url": "https://twitter.com/___adams__/status/1656942733771063296",
+					"display_url": "twitter.com/___adams__/sta…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden ⏏️",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist for @HTSI. Rapidly ageing musician. Play with Scritti Politti, Kenny Process Team, Lost Crowns, Sweet Billy Pilgrim.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72237,
+			"friends_count": 7125,
+			"listed_count": 917,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 39794,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 139952,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1656942733771063300,
+		"quoted_status_id_str": "1656942733771063296",
+		"quoted_status_permalink": {
+			"url": "https://t.co/z3HUxqEoYw",
+			"expanded": "https://twitter.com/___adams__/status/1656942733771063296",
+			"display": "twitter.com/___adams__/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 12 08:41:46 +0000 2023",
+			"id": 1656942733771063300,
+			"id_str": "1656942733771063296",
+			"full_text": "Sorcerer in swimmers! Ferdy Roberts, unforgettable as Prospero at Shakespeare’s Globe last year - now the lead in Ozma, rescuing a jellyfish! The film premieres at London’s Prince Charles Cinema on June 2nd with Q&amp;A – part of @scifilondon… https://t.co/NLM0SwB5MC @The_Globe https://t.co/XVvAgjL9y1",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "scifilondon",
+						"name": "SCI FI LONDON Film Festival",
+						"id": 18851704,
+						"id_str": "18851704",
+						"indices": [
+							230,
+							242
+						]
+					},
+					{
+						"screen_name": "The_Globe",
+						"name": "Shakespeare’s Globe",
+						"id": 17898494,
+						"id_str": "17898494",
+						"indices": [
+							268,
+							278
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/NLM0SwB5MC",
+						"expanded_url": "http://sci-fi-london.com/ozma",
+						"display_url": "sci-fi-london.com/ozma",
+						"indices": [
+							244,
+							267
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1656942587494760400,
+						"id_str": "1656942587494760449",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/media/Fv6kgSTXgAEy-bl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fv6kgSTXgAEy-bl.jpg",
+						"url": "https://t.co/XVvAgjL9y1",
+						"display_url": "pic.twitter.com/XVvAgjL9y1",
+						"expanded_url": "https://twitter.com/___adams__/status/1656942733771063296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 420,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 868,
+								"h": 536,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 868,
+								"h": 536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1656942587494760400,
+						"id_str": "1656942587494760449",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/media/Fv6kgSTXgAEy-bl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fv6kgSTXgAEy-bl.jpg",
+						"url": "https://t.co/XVvAgjL9y1",
+						"display_url": "pic.twitter.com/XVvAgjL9y1",
+						"expanded_url": "https://twitter.com/___adams__/status/1656942733771063296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 420,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 868,
+								"h": 536,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 868,
+								"h": 536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 216728505,
+				"id_str": "216728505",
+				"name": "Keith John Adams",
+				"screen_name": "___adams__",
+				"location": "",
+				"description": "Dreamer of dreams, films & music.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 22,
+				"friends_count": 107,
+				"listed_count": 0,
+				"created_at": "Wed Nov 17 15:12:38 +0000 2010",
+				"favourites_count": 7,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 14,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1656598990777987073/dr0OpPST_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1656598990777987073/dr0OpPST_normal.jpg",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I would urge lovers of wonderfully unusual films made on shoestring budgets to pop along to see Keith's film next month. A glorious achievement! ♥️ https://t.co/z3HUxqEoYw"
+	},
+	{
 		"created_at": "Thu May 11 18:11:25 +0000 2023",
 		"id": 1656723700652056600,
 		"id_str": "1656723700652056576",
