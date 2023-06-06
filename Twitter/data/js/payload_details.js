9a2a4fc9656c6daa1181016de5850acc8c49d305
freefrench <34857852+freefrench@users.noreply.github.com>
2023-06-06 01:19:33 +0100

    Added 1 tweets

--- Twitter/data/js/payload_details.js
+++ Twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 139869,
-	"created_at": "Mon Jun 05 00:19:32 +0000 2023",
+	"tweets": 139870,
+	"created_at": "Tue Jun 06 00:19:32 +0000 2023",
 	"lang": "en"
 }
--- Twitter/data/js/tweets/2023_06.js
+++ Twitter/data/js/tweets/2023_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Mon Jun 05 20:17:36 +0000 2023",
+		"id": 1665815153185570800,
+		"id_str": "1665815153185570820",
+		"full_text": "@sandersonjones @ImogenMoore hi Sanderson. I saw this on LinkedIn for some reason, and it touched me hugely. I don't know you but I hope you're OK. X",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sandersonjones",
+					"name": "Sanderson Jones",
+					"id": 13388092,
+					"id_str": "13388092",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "ImogenMoore",
+					"name": "Imogen Moore",
+					"id": 1502695441628082200,
+					"id_str": "1502695441628082183",
+					"indices": [
+						16,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1665694015344324600,
+		"in_reply_to_status_id_str": "1665694015344324611",
+		"in_reply_to_user_id": 13388092,
+		"in_reply_to_user_id_str": "13388092",
+		"in_reply_to_screen_name": "sandersonjones",
+		"user": {
+			"id": 1524841,
+			"id_str": "1524841",
+			"name": "Rhodri Marsden ⏏️",
+			"screen_name": "rhodri",
+			"location": "London, United Kingdom",
+			"description": "Vapid Response Unit. Writer. Tech columnist for @HTSI. Rapidly ageing musician. Play with Scritti Politti, Kenny Process Team, Lost Crowns, Sweet Billy Pilgrim.",
+			"url": "https://t.co/Vva73DDUbR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vva73DDUbR",
+							"expanded_url": "http://rhodri.info",
+							"display_url": "rhodri.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72220,
+			"friends_count": 7124,
+			"listed_count": 916,
+			"created_at": "Mon Mar 19 16:29:51 +0000 2007",
+			"favourites_count": 40011,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 140072,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756800733954072576/HWEM5rSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1524841/1517912072",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "424242",
+			"profile_text_color": "999999",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@sandersonjones @ImogenMoore hi Sanderson. I saw this on LinkedIn for some reason, and it touched me hugely. I don't know you but I hope you're OK. X"
+	},
+	{
 		"created_at": "Sun Jun 04 20:14:06 +0000 2023",
 		"id": 1665451886332326000,
 		"id_str": "1665451886332325890",
